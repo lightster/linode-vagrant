c74bda2098ae4e478d7f9568b9e6c4a224c20ac2
Matt Light <matt.light@illuminateed.com>
2015-04-20 01:21:55 -0400

    Do not checkout server-config during Vagrant bootstrap
    server-config repo is obsolete

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,6 @@ set -e
 set -u
 set -x
 
-# install server-config repo (needed for chef cookbooks)
 RETURN_DIR=$(pwd)
-mkdir -p /etc/lightdatasys
-cd /etc/lightdatasys
-if [ ! -d server/.git ]; then
-    git clone https://github.com/lightster/server-config.git server
-fi
-cd server
-chown -R vagrant:vagrant /etc/lightdatasys
-git remote set-url origin git@github.com:lightster/server-config.git
+
 cd $RETURN_DIR
