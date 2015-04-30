69a2ca928fdb35ce97261e7b216369bc9ad6afc4
Matt Light <matt.light@illuminateed.com>
2015-04-30 00:47:33 -0700

    Add boxkeeper user
    Boxkeeper will eventually be used for deployments

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,17 @@ set -e
 set -u
 set -x
 
+KEEPER_USERNAME="boxkeeper"
+
 RETURN_DIR=$(pwd)
+
+if id -u "$KEEPER_USERNAME" >/dev/null 2>&1; then
+    echo -n "User '$KEEPER_USERNAME' already exists... skipping"
+else
+    echo -n "Creating '$KEEPER_USERNAME' user..."
+    useradd -G wheel $KEEPER_USERNAME
+    echo "$KEEPER_USERNAME:vagrant" | chpasswd
+    echo " done"
+fi
 
 cd $RETURN_DIR
