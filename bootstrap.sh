2ba1dc7086146af2d2578bd0384328df46fb08de
Matt Light <matt.light@illuminateed.com>
2015-04-30 22:22:54 -0700

    Install boxkeeper SSH keys when provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ set -u
 set -x
 
 KEEPER_USERNAME="boxkeeper"
+KEEPER_HOMEDIR="/home/$KEEPER_USERNAME"
+KEEPER_SSHDIR="$KEEPER_HOMEDIR/.ssh"
+KEEPER_KEYS="$KEEPER_SSHDIR/authorized_keys"
 
 RETURN_DIR=$(pwd)
 
@@ -16,5 +19,14 @@ else
     echo "$KEEPER_USERNAME:vagrant" | chpasswd
     echo " done"
 fi
+
+mkdir -p $KEEPER_SSHDIR
+chmod 0700 $KEEPER_SSHDIR
+touch $KEEPER_KEYS
+chmod 0600 $KEEPER_KEYS
+curl -sS https://raw.githubusercontent.com/lightster/.ssh/master/id_rsa.lightster-air.pub \
+    https://raw.githubusercontent.com/lightster/.ssh/master/id_rsa.lightster-air.pub \
+    > $KEEPER_KEYS
+chown -R $KEEPER_USERNAME:$KEEPER_USERNAME $KEEPER_SSHDIR
 
 cd $RETURN_DIR
